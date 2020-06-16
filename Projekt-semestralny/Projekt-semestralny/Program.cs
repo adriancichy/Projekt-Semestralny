c1520317a3b1010c8f06241d207ef61546e9fc2e
Adik <Adik@DESKTOP-QC9EEKE>
2020-06-16 20:56:56 +0200

    Projektowanie gry Blackjack

---
+++ Projekt-semestralny/Projekt-semestralny/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projekt_semestralny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze blackjack");
+        }
+    }
+}
